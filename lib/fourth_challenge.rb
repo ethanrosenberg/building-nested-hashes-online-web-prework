f4f6cd35e1f53b9a6ab7eeb82cc9a114b19112c4
Ethan Rosenberg <ethan@ethanrosenberg.com>
2018-11-26 07:25:13 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -23,12 +23,12 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {
-          :name => "Benvolio",
-          :age => "17",
-          :attitude => "worried"
+          :name => "Steven ",
+          :age => "30",
+          :attitude => "confused"
         },
         {
-          :name => "Mercutio",
+          :name => "Nurse",
           :age => "18",
           :attitude => "hot-headed"
         }
